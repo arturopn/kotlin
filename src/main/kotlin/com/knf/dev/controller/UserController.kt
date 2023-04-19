5fd74ff914d67fc51a2fe9498000bb27aec93da5
arturoitnora <122922316+Arturo-itnora@users.noreply.github.com>
2023-04-18 20:05:37 -0600

    port change

--- src/main/kotlin/com/knf/dev/controller/UserController.kt
+++ src/main/kotlin/com/knf/dev/controller/UserController.kt
@@ -11,7 +11,7 @@ import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = arrayOf("http://0.0.0.0:$PORT"))
+@CrossOrigin(origins = arrayOf("http://0.0.0.0:4200"))
 class UserController(private val userRepository: UserRepository) {
 
     @GetMapping("/users")
