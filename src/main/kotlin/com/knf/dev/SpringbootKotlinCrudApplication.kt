844b935d19b08e7acb5ad43f4788c79a0cb43762
arturoitnora <122922316+Arturo-itnora@users.noreply.github.com>
2023-04-19 09:30:16 -0600

    Update SpringbootKotlinCrudApplication.kt

--- src/main/kotlin/com/knf/dev/SpringbootKotlinCrudApplication.kt
+++ src/main/kotlin/com/knf/dev/SpringbootKotlinCrudApplication.kt
@@ -1,16 +1,14 @@
-package com.knf.dev
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.filter.CorsFilter
-import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
 
 
 @SpringBootApplication
@@ -24,28 +22,21 @@ fun main(args: Array<String>) {
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/**")
             .addResourceLocations("classpath:/public/")
             .setCachePeriod(0)
     }
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .exposedHeaders("Authorization")
-    }
-
     @Bean
     fun corsFilter(): CorsFilter {
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
+        config.allowCredentials = true
         config.addAllowedOrigin("*")
         config.addAllowedHeader("*")
         config.addAllowedMethod("*")
-        config.addExposedHeader("Authorization")
         source.registerCorsConfiguration("/**", config)
         return CorsFilter(source)
     }
