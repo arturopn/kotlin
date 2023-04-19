b8a0933703d435e5b7b2e6ba92d35f4066faded3
arturoitnora <122922316+Arturo-itnora@users.noreply.github.com>
2023-04-19 06:38:56 -0600

    angularfront2

--- src/main/kotlin/com/knf/dev/SpringbootKotlinCrudApplication.kt
+++ src/main/kotlin/com/knf/dev/SpringbootKotlinCrudApplication.kt
@@ -2,13 +2,27 @@ package com.knf.dev
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
+import java.nio.file.Paths
 
 @SpringBootApplication
 class SpringbootKotlinCrudApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringbootKotlinCrudApplication>(*args){
-		setDefaultProperties(mapOf("server.address" to "0.0.0.0"));
-		app.use("/", express.static(path.join(__dirname, "angularfront")));
+	runApplication<SpringbootKotlinCrudApplication>(*args) {
+		setDefaultProperties(mapOf("server.address" to "0.0.0.0"))
+	}
+	
+	// Serve static files from the "dist" folder
+	@RestController
+	class StaticFileController {
+	    @GetMapping("/{path:[^\\.]*}")
+	    fun serveStaticFile(): ResponseEntity<Any> {
+	        val indexPath = Paths.get("angularfront/index.html")
+	        return ResponseEntity.ok().body(indexPath.toFile().readText())
+	    }
 	}
 }
