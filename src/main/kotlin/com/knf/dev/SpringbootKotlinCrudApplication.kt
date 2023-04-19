8a7412c6c9ca412b7f235f4611dd1512a26ba8bf
arturoitnora <122922316+Arturo-itnora@users.noreply.github.com>
2023-04-19 09:48:17 -0600

    Update SpringbootKotlinCrudApplication.kt

--- src/main/kotlin/com/knf/dev/SpringbootKotlinCrudApplication.kt
+++ src/main/kotlin/com/knf/dev/SpringbootKotlinCrudApplication.kt
@@ -2,12 +2,11 @@ package com.knf.dev
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.filter.CorsFilter
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.cors.CorsConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 
 
 @SpringBootApplication
@@ -27,19 +26,11 @@ class WebConfig : WebMvcConfigurer {
             .setCachePeriod(0)
     }
 
-    @Bean
-    fun corsFilter(): CorsFilter {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.addAllowedOrigin("*")
-        config.addAllowedMethod("GET")
-        config.addAllowedMethod("POST")
-        config.addAllowedMethod("PUT")
-        config.addAllowedMethod("DELETE")
-        config.addAllowedMethod("OPTIONS")
-        config.addAllowedHeader("*")
-        config.addExposedHeader("Authorization")
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("Authorization")
     }
 }
